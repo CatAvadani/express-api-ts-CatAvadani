57cec4fa05627ec39151e9fcd953690d148e378b
CatAvadani <catalinaavadani@gmail.com>
2024-04-15 23:11:42 +0200

    Use Zod for validation instead of Yup.

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,43 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
-import { validationSchema } from './data';
-
-export const validationProperties = [
-  'name',
-  'category',
-  'fields',
-  'price',
-  'amount',
-  'currency',
-  'subject',
-  'level',
-  'ratings',
-  'language',
-];
-
-export const validateProperties = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const keys = Object.keys(req.body);
-  const filteredKeys = keys.filter(
-    (key) => !validationProperties.includes(key)
-  );
-
-  if (filteredKeys.length > 0) {
-    return res.status(400).json({
-      message: 'Additional properties are not allowed',
-    });
-  }
-
-  next();
-};
+import { ZodSchema } from 'zod';
 
 export const validationMiddleware =
-  (schema: typeof validationSchema) =>
-  async (req: Request, res: Response, next: NextFunction) => {
+  (schema: ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
     try {
-      await schema.validate(req.body);
+      schema.parse(req.body);
       next();
     } catch (error: any) {
       res.status(400).json({ message: error.message });
